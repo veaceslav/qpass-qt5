04d1790eaef65660f95407c8665b0de3ff6f88b2
Mateusz Piekos <mateuszpiekos@gmail.com>
2011-02-15 11:16:59 +0100

    Corrected checking for other instances running
    QPass on unix use qlocalsocket and qlocalserver to determine if
    there is other instance running in the system.

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@
 #include <QSharedMemory>
 #include <QObject>
 #include <QTranslator>
+#include <QLocalSocket>
+#include <QLocalServer>
 
 #include "MainWindow.h"
 #include "PredefinedSettings.h"
@@ -31,30 +33,38 @@ int main(int argc, char *argv[])
 	QTranslator translator;
 	translator.load(PredefinedSettings::dataPath()+"/translations/"+locale);
 	app.installTranslator(&translator);
-	
+
+#ifdef Q_OS_UNIX
+	QLocalSocket socket;
+	socket.connectToServer("QPass");
+	if (socket.waitForConnected(500)) 
+	{
+		QMessageBox box;
+		box.setText( QObject::tr("One instance of QPass is already running!") );
+		box.setIcon( QMessageBox::Warning );
+		box.exec();
+		return 0; // Exit already a process running 
+	}
+
+	QLocalServer m_localServer;
+	m_localServer.removeServer("QPass");
+	int res = m_localServer.listen("QPass");
+#endif
+#ifndef Q_OS_UNIX
 	//Code to check if there is any running instance of QPass.
 	//It works very good on windows but on UNIX when you kill qpass
 	//process it doesn't remove this shared memory.
 	QSharedMemory memory("91628a41-6284-41a9-9b25-7b3cc365ced9"); 
 	if(!memory.create(1, QSharedMemory::ReadWrite))
 	{
-#ifndef Q_OS_UNIX
 		QMessageBox box;
 		box.setText( QObject::tr("One instance of QPass is already running!") );
 		box.setIcon( QMessageBox::Warning );
 		box.exec();
 		return 0;
-#endif
-#ifdef Q_OS_UNIX
-		QMessageBox box;
-		box.setText( QObject::tr("One instance of QPass is already running!\nIf you are sure that it isn't(it could occur when you kill process or application crashed) you can ignore this message, but it is dangerous to your database to have more than one instance running!") );
-		box.setStandardButtons( QMessageBox::Close | QMessageBox::Ignore);
-		box.setIcon( QMessageBox::Warning );
-		if(box.exec() == QMessageBox::Close)
-	return 0;
-#endif
 	}
-	
+#endif
+
 	MainWindow mainWindow;
 	
 	return app.exec();
